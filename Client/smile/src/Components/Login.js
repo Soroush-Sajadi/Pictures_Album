140befd3a2fe7ffc0beb0a7ff7de11f1870f0dcd
Soroush-Sajadi <soorooshsadjadee@gmail.com>
2020-08-25 15:24:34 +0200

    login page css

--- Client/smile/src/Components/Login.js
+++ Client/smile/src/Components/Login.js
@@ -1,23 +1,50 @@
 import React,{useReducer} from 'react';
+import  '../Style/Login.css'
 
 const Login = () => {
     const [ userIn, setUserIn ] = useReducer(
         (state, newState) => ({...state, ...newState}),
         {
-        firstPass: '',
-        secondPass: '',
+        email: '',
+        password: '',
         }
       );
-      const handleChangePass = evt => {
+
+    const handleChangePass = evt => {
         const name = evt.target.name;
         const newValue = evt.target.value;
         setUserIn({[name]: newValue});
     }
-    console.log(userIn)
+
+    const logIn = () => {
+        console.log('log in')
+    }
+
+    const signIn = () => {
+        console.log('sign in')
+    }
     return(
-        <div>
-            <input  className="login" type="email" name="firstPass" value={userIn.firstPass} placeholder="First password" onChange={ handleChangePass } />
-            <input  className="login" type="password" name="secondPass" value={userIn.secondPass} placeholder="Second password" onChange={ handleChangePass } />
+        <div className="login-wrapper">
+            <div className="login-left">
+                <h1>Smile</h1>
+            </div>
+            <div className="login-right">
+                <div className="login-right-title">
+                    <h3>
+                        Let's do it together
+                    </h3>
+                </div>
+                <div className="login-right-wrapper">
+                <div className="login-right-inputs">
+                    <input className="login-input" type="email" name="email" value={userIn.email} placeholder="Email*" onChange={ handleChangePass } />
+                    <input className="login-input" type="password" name="password" value={userIn.password} placeholder="Password*" onChange={ handleChangePass } />
+                </div>
+                <div className="login-right-buttons">
+                    <input className="login-login" type="submit" value="Log in" onClick={logIn}/>
+                    <input className="login-signin" type="submit" value="Sign in" onClick={signIn}/>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
