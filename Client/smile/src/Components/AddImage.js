46574e2453f76fec6f64048e1e73f668e6144d88
Soroush-Sajadi <soorooshsadjadee@gmail.com>
2020-08-31 21:43:30 +0200

    css

--- Client/smile/src/Components/AddImage.js
+++ Client/smile/src/Components/AddImage.js
@@ -8,21 +8,26 @@ import '../Style/AddImage.css'
 const AddImage = () => {
     const [ openAddImageWindow, setOpenAddImageWindow ] = useState(false);
 
-    const addingImage = () => {
-        setOpenAddImageWindow(true)
+    const openAddImageFolder = () => {
+        setOpenAddImageWindow(!openAddImageWindow)
     }
 
     return(
         <div className="add-image-wrapper">
             <div>
-                <img className="profile-add-image" src={addImage} alt="add image" onClick={addingImage}/>
+                <img className="profile-add-image" src={addImage} alt="add image" onClick={openAddImageFolder}/>
             </div>
+            { openAddImageWindow ? 
             <div className="add-image-window-wrapper">
-                <img className="add-image-window-wrapper-close-image" src={closeIcon} alt="close" /> 
+                <img className="add-image-window-wrapper-close-image" src={closeIcon} alt="close" onClick={openAddImageFolder}/> 
                 <img className="add-image-window-wrapper-add-img" src={addImageWindow} alt="add image" />
                 <textarea className="add-image-window-wrapper-description" ></textarea>
-                <input className="add-image-window-wrapper-date" type="date" id="birthday" name="When"></input>
+                <input className="add-image-window-wrapper-date" type="date" id="birthday" name="When"/>
+                <input className="add-image-window-wrapper-button" type="submit" value="Save"/>
             </div>
+            :
+            null
+            }
         </div>
     )
 }
